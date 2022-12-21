cecc8f8ea41429a714398a1d4f8f32c0447a8e9d
momo228860 <85031209+momo228860@users.noreply.github.com>
2022-12-21 15:13:44 +0800

    update file

--- hola.py
+++ hola.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hola_ubunlog():
-    return 'Hola Ubunlog'
+    return 'XDDDDD'
 
 if __name__ == "__main__":
         app.run(host='0.0.0.0')
